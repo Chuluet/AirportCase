b762022d155b89a618a7b527626efae0b9cbb054
Chuluet <santiagotoromedina@gmail.com>
2025-03-26 18:50:05 -0500

    feat: airportServices model, controller, migration and routes

---
+++ migrations/20250326234655-create-airportService.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("airportServices", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      locarion: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      isActive: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("airportServices");
+  },
+};
+
---
+++ models/airportServices.js
@@ -0,0 +1,47 @@
+'use strict';
+const { Model, DataTypers, DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+    class AirportServices extends Model {
+        static associate(models) {
+            // Definir asociaciones aquí si es necesario
+        }
+    }
+    AirportServices.init({
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        type: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        locarion: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        }
+
+    },
+        {
+            sequelize,
+            modelName: "AirportServices",
+            tableName: "airportServices", // Especificar nombre de la tabla
+            timestamps: true, // Agrega createdAt y updatedAt
+        }
+
+    )
+    return AirportServices
+}
